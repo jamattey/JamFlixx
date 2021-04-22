7ea0a8ce6b8e41d5dfc6e5edb5752a66e45855b4
James Amattey <69109175+jamattey@users.noreply.github.com>
2021-04-22 03:43:27 +0000

    Add project files.

---
+++ JamFlixx/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using JamFlixx.Data;
+using RazorPageMovie.Models;
+
+namespace JamFlixx.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly JamFlixx.Data.JamFlixxContext _context;
+
+        public CreateModel(JamFlixx.Data.JamFlixxContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ JamFlixx/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JamFlixx.Data;
+using RazorPageMovie.Models;
+
+namespace JamFlixx.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly JamFlixx.Data.JamFlixxContext _context;
+
+        public IndexModel(JamFlixx.Data.JamFlixxContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        // The BindProperty binds foreign values and query strings of the same name to the searchstring property
+        
+        [BindProperty(SupportsGet = true)]
+
+        // supportGet attribute is responsible for binding on get requests.
+
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
